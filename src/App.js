d5b31cbef521da92137580677970437be88c9b54
Su Htur Hlaing <shh.zotefamily@gmail.com>
2022-06-30 17:18:33 +0630

    [code]

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import JSONDATA from './MOCK_DATA.json'
+import {useState} from 'react'
+
+function App() {
+  const [searchTerm , setSearchTerm] = useState("");
+
+  return (
+    <div className="App">
+      <input type="text" placeholder='Search ...' 
+        onChange={(event) => {
+          setSearchTerm(event.target.value); 
+        }} 
+      />
+      {
+        JSONDATA.filter((val) => {
+          if (searchTerm == "") {
+            return val
+          }else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+            return val
+          }
+        }).map((val, key) => {
+          return <div className='user' key={key}>
+                      <p>
+                        {val.first_name}
+                      </p>
+                      
+                  </div>
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
